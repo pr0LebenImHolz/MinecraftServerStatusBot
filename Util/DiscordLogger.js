de99adca67dc609716dd758c6026e778c4ae84eb
fivekWBassMachine <53333237+fivekWBassMachine@users.noreply.github.com>
2021-07-19 21:37:41 +0200

    FIX stupid

--- Util/DiscordLogger.js
+++ Util/DiscordLogger.js
@@ -9,10 +9,10 @@ class DiscordLogger {
 			channel.send(message);
 		});
 	}
-	static enum LogLevels {
-		EVERYTHING: 0, // Logs API and Discord (command) events
-		API_EVERYTHING: 1, // Logs all API events (server starting/ started/ stopping/ stopped, player joined/ left)
-		API_SERVER: 2 // Logs server API events (server starting/ started/ stopping/ stopped)
-	}
+}
+DiscordLogger.LogLevels = {
+	EVERYTHING: 0, // Logs API and Discord (command) events
+	API_EVERYTHING: 1, // Logs all API events (server starting/ started/ stopping/ stopped, player joined/ left)
+	API_SERVER: 2 // Logs server API events (server starting/ started/ stopping/ stopped)
 }
 module.exports = DiscordLogging;
